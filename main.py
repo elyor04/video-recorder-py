ada7ba06f8fc289518a1d0dea7c55d26468eebf3
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2023-08-10 21:45:01 +0500

    just commit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from PyQt6.QtWidgets import QApplication
 from AppMainWindow import AppMainWindow
 from sys import exit as sys_exit, argv as sys_argv
 
-
 if __name__ == "__main__":
     app = QApplication(sys_argv)
     win = AppMainWindow()
