58af7c083488c06f6c0cbec6b80cbd20b28f77c5
Elyor Tukhtamuratov <tuxtamuratovelyor@gmail.com>
2023-08-23 16:01:02 +0500

    just commit

--- appRun.sh
+++ appRun.sh
@@ -5,6 +5,8 @@ BASE_DIR=$(dirname "$(readlink -f "$0")")
 platform=$(echo "$(uname -s)" | grep -o "^[a-zA-Z0-9_]*")
 arch=$(uname -m)
 
+isLinux=false
+
 if [ "$platform" = "Linux" ]; then
     if [ "$arch" = "x86_64" ]; then
         echo "Linux 64-bit"
@@ -14,7 +16,6 @@ if [ "$platform" = "Linux" ]; then
         echo "Linux 32-bit"
         export LD_LIBRARY_PATH="$BASE_DIR/HKIPcamera/libs/linux32:$LD_LIBRARY_PATH"
         export LD_LIBRARY_PATH="$BASE_DIR/HKIPcamera/libs/linux32/HCNetSDKCom:$LD_LIBRARY_PATH"
-    python3 "$BASE_DIR/main.py" "$@"
     fi
 elif [ "$platform" = "Windows" ] || [ "$platform" = "Windows_NT" ] || [ "$platform" = "MSYS_NT" ] || [ "$platform" = "CYGWIN" ] || [ "$platform" = "MINGW64_NT" ] || [ "$platform" = "MINGW32_NT" ]; then
     if [ "$arch" = "x86_64" ]; then
@@ -25,9 +26,14 @@ elif [ "$platform" = "Windows" ] || [ "$platform" = "Windows_NT" ] || [ "$platfo
         echo "Windows 32-bit"
         export PATH="$BASE_DIR/HKIPcamera/libs/win32:$PATH"
         export PATH="$BASE_DIR/HKIPcamera/libs/win32/HCNetSDKCom:$PATH"
-    python "$BASE_DIR/main.py" "$@"
     fi
 else
     echo "Hikvision Net SDK does not support this platform"
     exit 0
+fi
+
+if [ "$isLinux" ]; then
+    python3 "$BASE_DIR/main.py" "$@"
+else
+    python "$BASE_DIR/main.py" "$@"
 fi
